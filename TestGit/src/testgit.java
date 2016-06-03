c9df3c728884e11501c493955fcb70643fe33030
Viktoriia Schwartz <viktoriiaschwartz@gmail.com>
2016-06-02 22:52:08 -0700

    # WARNING: head commit changed in the meantime
    third change
    
    Signed-off-by: Viktoriia Schwartz <viktoriiaschwartz@gmail.com>

---
+++ TestGit/src/testgit.java
@@ -0,0 +1,9 @@
+
+public class testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ USACO-2016/src/namenum.java
@@ -0,0 +1,9 @@
+
+public class namenum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
